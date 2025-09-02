841ec6ea9b082903afcce3e28b0e49da71918a99
arefatmoh <arefat.mohammed-ug@aau.edu.et>
2025-09-02 23:45:18 +0300

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("66633003b80eca8c")
+/******/ 		__webpack_require__.h = () => ("3586dd431e5d1557")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
